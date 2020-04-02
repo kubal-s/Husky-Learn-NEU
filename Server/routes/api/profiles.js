ce5b9f386996b60c3bdd5fcd33ee76dd42155cf2
Sumesh Harale <harale.s@husky.neu.edu>
2020-04-01 21:22:13 -0400

    Completed endpoint to GET profile of User

--- Server/routes/api/profiles.js
+++ Server/routes/api/profiles.js
@@ -5,3 +5,28 @@ const auth = require('../auth');
 
 
 module.exports = router;
+
+// PrePopulate profile when username parameter is contained within a route.
+router.param('username', function(req, res, next, username){
+  User.findOne({username: username}).then(function(user){
+    if (!user) { return res.sendStatus(404); }
+
+    req.profile = user;
+
+    return next();
+  }).catch(next);
+});
+
+// Endpoint to fetch User's Profile by their username
+router.get('/:username', auth.optional, function(req, res, next){
+  if(req.payload){
+    User.findById(req.payload.id).then(function(user){
+      if(!user){
+        return res.json({profile:req.profile.toProfileJSONFor(false)});
+      }
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }else{
+    return res.json({profile: req.profile.toProfileJSONFor(false)});
+  }  
+});

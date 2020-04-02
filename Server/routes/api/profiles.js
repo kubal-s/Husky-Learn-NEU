9c948c9cc8e851b1ccadeb900b6e6f5a51797cae
kubal-s <kubal.s@husky.neu.edu>
2020-04-02 19:46:04 -0400

    unfollow a user

--- Server/routes/api/profiles.js
+++ Server/routes/api/profiles.js
@@ -42,3 +42,15 @@ router.post('/:username/follow', auth.required, function(req, res, next){
     });
   }).catch(next);
 });
+//unfollow a user 
+router.delete('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.unfollow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});

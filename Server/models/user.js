620d544c01608b179e0946cffa6b062c024972ad
kubal-s <kubal.s@husky.neu.edu>
2020-04-02 19:37:16 -0400

    added method to unfollow a user

--- Server/models/user.js
+++ Server/models/user.js
@@ -101,4 +101,9 @@ UserSchema.methods.follow = function(id){
 
   return this.save();
 };
+//unfollow a user 
+UserSchema.methods.unfollow = function(id){
+  this.following.remove(id);
+  return this.save();
+};
 mongoose.model('User', UserSchema);

cef80f917df3ae1f6202beb9eadba66623aa305d
kubal-s <kubal.s@husky.neu.edu>
2020-04-03 19:22:05 -0400

    signup component and styling added

--- husky-learn/src/app/app.module.ts
+++ husky-learn/src/app/app.module.ts
@@ -9,12 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon'
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +34,12 @@ import { SignInComponent } from './sign-in/sign-in.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- husky-learn/src/app/sign-up/sign-up.component.ts
+++ husky-learn/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, Validators,FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  hide = true;
+  signupForm: FormGroup;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      email:  this.email,
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   }
 
+  
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  onSubmit(){
+    console.log("logged")
+  }
 }

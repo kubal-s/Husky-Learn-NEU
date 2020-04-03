e13956814a62bd8a258bcc38151446984f4ac55b
Vardhana-aed <bhatt.va@husky.neu.edu>
2020-04-03 01:39:07 -0400

    added method to get all tags used

--- Server/routes/api/tags.js
+++ Server/routes/api/tags.js
@@ -3,3 +3,10 @@ const mongoose = require('mongoose');
 const Article = mongoose.model('Article');
 
 module.exports = router;
+
+//Creating a route for getting set of tags used on an article
+router.get('/', function (req, res, next) {
+    Article.find().distinct('tagList').then(function (tags) {
+        return res.json({ tags: tags });
+    }).catch(next);
+});

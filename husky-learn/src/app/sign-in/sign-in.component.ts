9b3d3f321e88267033b0d8c0f89c1b10bbe66e94
kubal-s <kubal.s@husky.neu.edu>
2020-04-03 19:29:44 -0400

    added sign in and proper routing

--- husky-learn/src/app/sign-in/sign-in.component.ts
+++ husky-learn/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  signinForm: FormGroup;
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    this.signinForm = new FormGroup({
+      email:  this.email,
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   }
 
+  
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  onSubmit(){
+    console.log("logged")
+  }
 }

2706ff8ce6c420a1a52f3559acb1ce6187e91809
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2020-04-09 16:13:13 -0400

    Settings form updated and user service created

--- husky-learn/src/app/components/settings/settings.component.ts
+++ husky-learn/src/app/components/settings/settings.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authservices/auth.service';
 import { ProfileService } from '../../services/userservices/profile.service';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 import { User } from 'src/app/model/User';
-import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/userservices/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,20 +12,41 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  user: User = {} as User;
+  settingsForm: FormGroup;
+  errors: Object = {};
+  isSubmitting = false;
 
-
-  constructor(private authService:AuthService,private profileService:ProfileService) { }
-
-  ngOnInit(): void {
-    this.profileService.getUser().subscribe(
-      data => {
-        //console.log(data)
-      },
-      err => {
-        //console.log(err)
+  constructor( private router: Router,
+    private userService: UserService,
+    private fb: FormBuilder,
+    private authService:AuthService) { 
+      this.settingsForm = this.fb.group({
+        image: '',
+        username: '',
+        bio: '',
+        email: '',
+        password: ''
       });
+      // Optional: subscribe to changes on the form
+      // this.settingsForm.valueChanges.subscribe(values => this.updateUser(values));
+    }
+    
+  ngOnInit(): void {
+    // Make a fresh copy of the current user's object to place in editable form fields
+    Object.assign(this.user, this.userService.getCurrentUser());
+    // Fill the form
+    this.settingsForm.patchValue(this.user);
   }
-  logOut(){
+
+  logout() {
     this.authService.logout();
   }
+
+
+
+  updateUser(values: Object) {
+    Object.assign(this.user, values);
+  }
+
 }
---
+++ husky-learn/src/app/services/userservices/user.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { User } from 'src/app/model/User';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+import { ReplaySubject, Observable } from 'rxjs';
+import { ApiService } from 'src/app/sharedservices/http-request-custom';
+import { HttpClient } from '@angular/common/http';
+import { JwtService } from 'src/app/sharedservices/jwtToken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private currentUserSubject = new BehaviorSubject<User>({} as User);
+  public currentUser = this.currentUserSubject.asObservable().pipe(distinctUntilChanged());
+
+  private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
+  public isAuthenticated = this.isAuthenticatedSubject.asObservable();
+  constructor(
+    private apiService: ApiService,
+    private http: HttpClient,
+    private jwtService: JwtService
+  ) { }
+
+  populate() {
+    // If JWT detected, attempt to get & store user's info
+    if (this.jwtService.getToken()) {
+      this.apiService.get('/user')
+      .subscribe(
+        data => this.setAuth(data.user),
+        err => this.purgeAuth()
+      );
+    } else {
+      // Remove any potential remnants of previous auth states
+      this.purgeAuth();
+    }
+  }
+  purgeAuth(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  setAuth(user: User) {
+    // Save JWT sent from server in localstorage
+    this.jwtService.saveToken(user.token);
+    // Set current user data into observable
+    this.currentUserSubject.next(user);
+    // Set isAuthenticated to true
+    this.isAuthenticatedSubject.next(true);
+  }
+
+
+
+  attemptAuth(type, credentials): Observable<User> {
+    const route = (type === 'login') ? '/login' : '';
+    return this.apiService.post('/users' + route, {user: credentials})
+      .pipe(map(
+      data => {
+        this.setAuth(data.user);
+        return data;
+      }
+    ));
+  }
+
+  getCurrentUser(): User {
+    return this.currentUserSubject.value;
+  }
+
+  // Update the user on the server (email, pass, etc)
+  update(user): Observable<User> {
+    return this.apiService
+    .put('/user', { user })
+    .pipe(map(data => {
+      // Update the currentUser observable
+      this.currentUserSubject.next(data.user);
+      return data.user;
+    }));
+  }
+
+}
